dc1ed51ba00d25c4951cc9e2adb359781038a691
duland94 <54681510+duland94@users.noreply.github.com>
2020-08-30 02:15:34 -0500

    Update main.cpp

--- Assignments/A03/#11498/main.cpp
+++ Assignments/A03/#11498/main.cpp
@@ -19,29 +19,29 @@ int main(){
   ifstream Cin;
   Cin.open("input.txt");
 
-  int K,X,y, divX,divY;
+  int K,X,Y, divX,divY;
 
   Cin>>K;
   while(K!=0){
     Cin>> divX >> divY;
 
     for(int i=0;i<K;i++){
-        Cin>> X >> y;
+        Cin>> X >> Y;
 
         if(X > divX)
       {
-        if(y > divY)
+        if(Y > divY)
           cout << "NE" << endl;
-        else if(y < divY)
+        else if(Y < divY)
           cout << "SE" << endl;
         else 
           cout << "divisa" << endl;
       }
       else if(X < divX)
       {
-        if(y > divY)
+        if(Y > divY)
           cout << "NO" << endl;
-        else if(y < divY)
+        else if(Y < divY)
           cout << "SO" << endl;
         else 
           cout << "divisa" << endl;

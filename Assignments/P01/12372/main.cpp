61541941752f7e572a8dfe3af476ca88c2f24aa0
duland94 <54681510+duland94@users.noreply.github.com>
2020-10-09 17:16:31 -0500

    Update main.cpp

--- Assignments/P01/12372/main.cpp
+++ Assignments/P01/12372/main.cpp
@@ -12,25 +12,21 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
-#include <fstream>
 
 using namespace std;
 
 int main(){
-  ifstream Cin;
-  Cin.open("input.txt");
-
   int T;
   int ln;
   int wd;
   int ht;
 
-  Cin >> T;
+  cin >> T;
 
   for(int i = 0; i < T; i++){
-    Cin >> ln;
-    Cin >> wd;
-    Cin >> ht;
+    cin >> ln;
+    cin >> wd;
+    cin >> ht;
 
     if((ln>20) || (wd>20) || (ht>20)){
       cout << "Case " << i+1 << ": bad\n";
